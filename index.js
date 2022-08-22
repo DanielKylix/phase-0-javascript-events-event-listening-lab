fbd04732329d7db0bda8f9f38ef8f4ebd1e29234
Daniel <daniel@kylix.online>
2022-08-22 14:51:00 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-function addingEventListener() {
-    alert('I was clicked!');
+const input = document.getElementById("input");
+
+function clickAlert() {
+  alert("I was clicked!");
 }
 
-const button = document.getElementById('button')
-button.addEventListener('click',addingEventListener)
+input.addEventListener("click", clickAlert);
